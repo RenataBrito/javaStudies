2b1c011c77d4348231ab91c6ec51a03f0255846a
RenataBrito <renata.oliveira.brito@gmail.com>
2025-01-01 20:46:04 -0300

    first commint of 2025

---
+++ src/main/java/springStudies/twentyFive/TwentyFiveApplication.java
@@ -0,0 +1,13 @@
+package springStudies.twentyFive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwentyFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwentyFiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springStudies/twentyFive/TwentyFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package springStudies.twentyFive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwentyFiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
